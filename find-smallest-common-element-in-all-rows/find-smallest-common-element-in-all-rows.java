7f9fb948f07a806f47f154d879be919f4c93a11d
ashutosh <ashutosh049@outlook.com>
2022-01-03 23:21:41 +0530

    Time: 2 ms (66.09%), Space: 60.5 MB (59.65%) - LeetHub

--- find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.java
+++ find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.java
@@ -1,11 +1,36 @@
 class Solution {
     public int smallestCommonElement(int[][] mat) {
         
+       //return freqTwoPass(mat);
+       return freqSinglePass(mat);
+        
+    }
+    
+     public int freqSinglePass(int[][] mat) {
+        
+        int[] freq = new int[10001];
+        int minElm = Integer.MAX_VALUE;
+        
+        for(int[] rElm: mat){
+           for(int cElm: rElm){
+               freq[cElm]++;
+               if(freq[cElm] == mat.length){
+                    minElm = Math.min(minElm, cElm);  
+               }
+            }   
+        }
+        
+        return minElm == Integer.MAX_VALUE ? -1 : minElm;
+        
+    }
+    
+    public int freqTwoPass(int[][] mat) {
+        
         int[] freq = new int[10001];
         
         for(int[] rElm: mat){
            for(int cElm: rElm){
-             freq[cElm]++;
+               freq[cElm]++;
             }   
         }
         
