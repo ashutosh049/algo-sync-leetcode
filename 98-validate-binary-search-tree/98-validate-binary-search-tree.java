684f8e9ddf459b7ecc4d3a6008d6319577122d53
ashutosh <ashutosh049@outlook.com>
2022-01-10 22:28:34 +0530

    Time: 0 ms (100.00%), Space: 38.3 MB (88.54%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return validate(root, null, null);
+        
+        return isValid(root, null, null);
     }
     
-      public boolean validate(TreeNode root, Integer low, Integer high) {
-        
-        if (root == null) {
+    private boolean isValid(TreeNode root, Integer low, Integer high){
+        if(root == null){
             return true;
         }
         
-        if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
+        if(low != null && root.val <= low){
             return false;
         }
         
-        return validate(root.left, low, root.val) && validate(root.right, root.val, high);
+        if(high !=null && root.val >= high){
+            return false;
+        }
+        
+        return isValid(root.left, low, root.val) && isValid(root.right, root.val, high);
+        
     }
 }
