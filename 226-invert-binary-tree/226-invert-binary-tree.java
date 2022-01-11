a91f100228f54932a0b66fa5a261b35f19374201
ashutosh <ashutosh049@outlook.com>
2022-01-11 07:04:04 +0530

    Time: 0 ms (100.00%), Space: 38.3 MB (24.60%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -1,7 +1,29 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        dfs(root);
+        //dfs(root);
+        //return root;
+        
+        return bfs(root);
+    }
+    
+    private TreeNode bfs(TreeNode root){
+        
+        if (root == null) return null;
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
+        
         return root;
+        
     }
     
     private void dfs(TreeNode root){
