12edb305c8bae2a29bc721459a7db4c4a2c9a763
ashutosh <ashutosh049@outlook.com>
2022-02-09 22:44:11 +0530

    Time: 451 ms (5.08%), Space: 42.5 MB (57.49%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -14,26 +14,13 @@ class Solution {
             
             for(int j=i+1; j<len; j++){
                 int operand2 = nums[j];
-                if(valid(operand1, operand2, k, pairSet)){
+                if(Math.abs(operand1 - operand2) == k){
                     pairSet.add(operand1 + operand2);
                 }
             }
             
         }
         return pairSet.size();
-    }
-    
-    /**
-    * TC: 
-    */
-    private boolean valid(int op1, int op2, int k, Set<Integer> pairSet){
-        
-        // if(Math.abs(op1 - op2) == k){
-        //     return !pairSet.contains(op1 + op2);
-        // }
-        // return false;
-        return Math.abs(op1 - op2) == k;
-        
     }
     
 }
