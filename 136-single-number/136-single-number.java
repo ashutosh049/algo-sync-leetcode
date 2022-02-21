d1cd04b04666eee9f7f7df4d157ff1641604884c
ashutosh <ashutosh049@outlook.com>
2022-02-21 20:30:28 +0530

    Time: 2 ms (58.84%), Space: 50.5 MB (38.81%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res=0;
+         
+        for(int i:nums)
+           res = res ^ i;
+        
+        return res;
+    }
+}
