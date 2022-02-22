9b835ea99fca596144a904e995cbdcbfa693b3c5
ashutosh <ashutosh049@outlook.com>
2022-02-23 05:14:50 +0530

    Time: 373 ms (16.89%), Space: 68.4 MB (15.78%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,12 +1,33 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-      int reach=0;
-        
-        for(int i=0;i<=reach;i++){
-            reach = Math.max(reach,i+nums[i]);
-            
-            if(reach>=nums.length-1) return true;
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
+    Index[] memo;
+
+    public boolean canJumpFromPosition(int position, int[] nums) {
+        if (memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD ? true : false;
         }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+
+        memo[position] = Index.BAD;
         return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+        return canJumpFromPosition(0, nums);
     }
 }
