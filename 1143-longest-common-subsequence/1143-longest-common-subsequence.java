5c8a49ba9358ec12de9fc8a3613d754920eac6ad
ashutosh <ashutosh049@outlook.com>
2022-02-28 20:14:45 +0530

    Time: 11 ms (83.46%), Space: 41.7 MB (96.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,51 +1,35 @@
 class Solution {
-    private String text1;
-    private String text2;
-    private int l1;
-    private int l2;
-    private Integer[][] memo;
-    private String seq;
-
-    public int longestCommonSubsequence(String text1, String text2) {
-        this.text1 = text1;
-        this.text2 = text2;
-        this.l1 = text1.length();
-        this.l2 = text2.length();
-        this.memo = new Integer[l1 + 1][l2 + 1];
-        this.seq = "";
+    
+  public int longestCommonSubsequence(String text1, String text2) {    
+    
+    // If text1 doesn't reference the shortest string, swap them.
+    if (text2.length() < text1.length()) {
+      String temp = text1;
+      text1 = text2;
+      text2 = temp;
+    }
+      
+    // The previous and current column starts with all 0's and like 
+    // before is 1 more than the length of the first word.
+    int[] previous = new int[text1.length() + 1];
+    int[] current = new int[text1.length() + 1];
+      
+    // Iterate through each column, starting from the last one.
+    for (int col = text2.length() - 1; col >= 0; col--) {
+      for (int row = text1.length() - 1; row >= 0; row--) {
+        if (text1.charAt(row) == text2.charAt(col)) {
+          current[row] = 1 + previous[row + 1];
+        } else {
+          current[row] = Math.max(previous[row], current[row + 1]);
+        }
+      }
+      // The current column becomes the previous one, and vice versa.
+      int[] temp = previous;
+      previous = current;
+      current = temp;
+    }
         
-        if (l1 == 0 || l2 == 0) {
-            return 0;
-        }
-        if (l1 == 1) {
-            return (text2.contains(text1)) ? 1 : 0;
-        } else if (l2 == 1) {
-            return (text1.contains(text2)) ? 1 : 0;
-        }
-
-        int maxLength = dp(0, 0);
-        System.out.println("seq: "+seq);
-        return maxLength;
-    }
-
-    private int dp(int i, int j) {
-        if (i == l1 || j == l2) {
-            return 0;
-        }
-
-        if (memo[i][j] == null) {
-            int maxLength = 0;
-
-            if (text1.charAt(i) == text2.charAt(j)) {
-                seq += text1.charAt(i);
-                maxLength = dp(i + 1, j + 1) + 1;
-            } else {
-                maxLength = Math.max(dp(i, j + 1), dp(i + 1, j));
-            }
-
-            memo[i][j] = maxLength;
-        }
-
-        return memo[i][j];
-    }
+    // The original problem's answer is in previous[0]. Return it.
+    return previous[0];
+  }
 }
