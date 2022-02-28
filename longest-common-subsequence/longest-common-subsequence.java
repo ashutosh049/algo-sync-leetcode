96c04ffec9d049bf9496c6416f8cf4f2e6493c43
ashutosh <ashutosh049@outlook.com>
2022-02-28 06:32:14 +0530

    Time: 42 ms, Memory: 65 MB - LeetHub

---
+++ longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,46 @@
+class Solution {
+    private String text1;
+    private String text2;
+    private int l1;
+    private int l2;
+    private Integer[][] memo;
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        this.text1 = text1;
+        this.text2 = text2;
+        this.l1 = text1.length();
+        this.l2 = text2.length();
+        this.memo = new Integer[l1 + 1][l2 + 1];
+
+        if (l1 == 0 || l2 == 0) {
+            return 0;
+        }
+        if (l1 == 1) {
+            return (text2.contains(text1)) ? 1 : 0;
+        } else if (l2 == 1) {
+            return (text1.contains(text2)) ? 1 : 0;
+        }
+
+        return dp(0, 0);
+    }
+
+    private int dp(int i, int j) {
+        if (i == l1 || j == l2) {
+            return 0;
+        }
+
+        if (memo[i][j] == null) {
+            int maxLength = 0;
+
+            if (text1.charAt(i) == text2.charAt(j)) {
+                maxLength = dp(i + 1, j + 1) + 1;
+            } else {
+                maxLength = Math.max(dp(i, j + 1), dp(i + 1, j));
+            }
+
+            memo[i][j] = maxLength;
+        }
+
+        return memo[i][j];
+    }
+}
