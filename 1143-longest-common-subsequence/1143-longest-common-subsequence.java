a46093e5338848a5d930b108ae632897b0ff33ee
ashutosh <ashutosh049@outlook.com>
2022-02-28 20:14:00 +0530

    Time: 81 ms (5.80%), Space: 57.3 MB (7.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,15 +4,17 @@ class Solution {
     private int l1;
     private int l2;
     private Integer[][] memo;
-    
+    private String seq;
+
     public int longestCommonSubsequence(String text1, String text2) {
         this.text1 = text1;
         this.text2 = text2;
         this.l1 = text1.length();
         this.l2 = text2.length();
-        this.memo = new Integer[l1+1][l2+1];
-
-        if(l1 ==0 || l2 == 0){
+        this.memo = new Integer[l1 + 1][l2 + 1];
+        this.seq = "";
+        
+        if (l1 == 0 || l2 == 0) {
             return 0;
         }
         if (l1 == 1) {
@@ -20,31 +22,30 @@ class Solution {
         } else if (l2 == 1) {
             return (text1.contains(text2)) ? 1 : 0;
         }
-    
-        return dp(0, 0);
+
+        int maxLength = dp(0, 0);
+        System.out.println("seq: "+seq);
+        return maxLength;
     }
-    
-    private int dp(int i, int j){
-        
-        if(i == l1 || j == l2){
+
+    private int dp(int i, int j) {
+        if (i == l1 || j == l2) {
             return 0;
         }
-        
-        if(memo[i][j] == null){
-            
-            int maxLength = 0;
-            
-            if(text1.charAt(i) == text2.charAt(j)){
-                maxLength =  dp(i+1, j+1) +1;
-            }else{
-                maxLength = Math.max(dp(i, j+1), dp(i+1, j));
-            }
-        
-            memo[i][j] = maxLength;
-                        
-        }
-        
-        return memo[i][j];
 
+        if (memo[i][j] == null) {
+            int maxLength = 0;
+
+            if (text1.charAt(i) == text2.charAt(j)) {
+                seq += text1.charAt(i);
+                maxLength = dp(i + 1, j + 1) + 1;
+            } else {
+                maxLength = Math.max(dp(i, j + 1), dp(i + 1, j));
+            }
+
+            memo[i][j] = maxLength;
+        }
+
+        return memo[i][j];
     }
 }
