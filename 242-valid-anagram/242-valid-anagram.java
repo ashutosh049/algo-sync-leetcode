172fe5435db3950f142362f3fafbc279d89d2dbc
ashutosh <ashutosh049@outlook.com>
2022-03-30 23:37:53 +0530

    Time: 5 ms (65.79%), Space: 43.4 MB (71.72%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,35 +1,33 @@
 class Solution {
+
     public boolean isAnagram(String s, String t) {
-                
         int len = s.length();
         boolean isAnagram = true;
-        
-        if(len != t.length()){
+
+        if (len != t.length()) {
             return false;
         }
-        
-        
+
         int[] freS = new int[26];
         int[] freT = new int[26];
-        
-        for(int i=0; i < len; i++){
+
+        for (int i = 0; i < len; i++) {
             int codePointS = (int) s.charAt(i);
             codePointS = codePointS % 97;
             freS[codePointS]++;
-            
+
             int codePointT = (int) t.charAt(i);
             codePointT = codePointT % 97;
             freT[codePointT]++;
         }
-        
-        for(int i=0; i < 26; i++){
-            if(freS[i] != freT[i]){
+
+        for (int i = 0; i < 26; i++) {
+            if (freS[i] != freT[i]) {
                 isAnagram = false;
                 break;
             }
         }
-        
+
         return isAnagram;
-        
     }
 }
