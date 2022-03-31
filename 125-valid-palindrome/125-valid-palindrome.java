62573f81336c330d9f36a7fc56d47c98f0972f76
ashutosh <ashutosh049@outlook.com>
2022-04-01 02:27:41 +0530

    Time: 5 ms (68.96%), Space: 43.6 MB (63.75%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,42 +1,41 @@
 class Solution {
+
     public boolean isPalindrome(String s) {
-        
         String str = lowercase(removeNonAlphanumeric(s));
-        
+
         int len = str.length();
-        
-        if(len == 0){
+
+        if (len == 0) {
             return true;
         }
-        
-        boolean isPalidrome =  true;
-        int i=0;
-        
-        while(i <= len/2){
-            if(str.charAt(i) == str.charAt(len -1 -i)){
+
+        boolean isPalidrome = true;
+        int i = 0;
+
+        while (i <= len / 2) {
+            if (str.charAt(i) == str.charAt(len - 1 - i)) {
                 i++;
-            }else{
+            } else {
                 isPalidrome = false;
                 break;
             }
         }
-        
+
         return isPalidrome;
-        
     }
-    
-    private String lowercase(final String str){
+
+    private String lowercase(final String str) {
         return str.toLowerCase();
     }
-    
-    private String removeNonAlphanumeric(final String str){
+
+    private String removeNonAlphanumeric(final String str) {
         StringBuilder sb = new StringBuilder();
-        
-        int i=0;
-        while(i < str.length()){
-            char c= str.charAt(i);
-            int cp = (int)c;
-            if((cp >= 65 && cp<= 90) || (cp >= 97 && cp <= 122) || (cp >= 48 && cp <= 57) ){
+
+        int i = 0;
+        while (i < str.length()) {
+            char c = str.charAt(i);
+            int cp = (int) c;
+            if ((cp >= 65 && cp <= 90) || (cp >= 97 && cp <= 122) || (cp >= 48 && cp <= 57)) {
                 sb.append(c);
             }
             i++;
