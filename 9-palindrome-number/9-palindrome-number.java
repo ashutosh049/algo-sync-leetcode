d45a0c87680fb3305d3f164ce6c0aa5c4dc1e828
ashutosh <ashutosh049@outlook.com>
2022-04-01 00:48:49 +0530

    Time: 6 ms (99.96%), Space: 41.7 MB (86.23%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,21 +1,24 @@
 class Solution {
+
     public boolean isPalindrome(int x) {
-        
         int original = x;
+
+        if (x < 0) return false;
+
+        int rev = reverse(x);
+
+        return original == rev;
+    }
+
+    private int reverse(int x) {
         int rev = 0;
-        
-        if(x < 0)
-            return false;
-        
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return false;
-            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return false;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
         }
-        
-        return original == rev;
-        
+        return rev;
     }
 }
