a6c2b3fe1bb199b0df6b4642c9fbc579d90de5dd
ashutosh <ashutosh049@outlook.com>
2022-04-01 00:55:26 +0530

    Time: 14 ms (50.56%), Space: 44.5 MB (59.17%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -16,9 +16,9 @@ class Solution {
             int pop = x % 10;
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            //rev = rev * 10 + pop;
-            rev *= 10;
-            rev += pop;
+            rev = rev * 10 + pop;
+            //rev *= 10;
+            //rev += pop;
             x /= 10;
         }
         return rev;
