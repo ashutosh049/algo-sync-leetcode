8c8f9aedb7e2ec71634356c2a7b487de29ec42a6
ashutosh <ashutosh049@outlook.com>
2022-04-03 02:50:52 +0530

    Time: 9 ms (75.43%), Space: 54.8 MB (29.54%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -8,24 +8,20 @@ class Solution {
         int right = len - 1;
 
         while (left < right) {
-            
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
-            
-            if (leftChar == rightChar) {
+            if (s.charAt(left) == s.charAt(right)) {
                 left++;
                 right--;
-            } else{
-                boolean leftSkipped =isPalidrome(s, left+1, right);
-                boolean rightSkipped =isPalidrome(s, left, right-1);
-                return (leftSkipped || rightSkipped);
+            } else {
+                // Get the first char which is unequal.
+                // Skip left char and check for palindrome and similarly, skip right and check
+                return (isPalidrome(s, left + 1, right) || isPalidrome(s, left, right - 1));
             }
-          
         }
 
         return isPalidrome;
     }
 
+    // Classic palidrome check using 2 pointer
     private boolean isPalidrome(String s, int left, int right) {
         int len = s.length();
 
@@ -40,9 +36,7 @@ class Solution {
         boolean isPalidrome = true;
 
         while (left < right) {
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
-            if (leftChar == rightChar) {
+            if (s.charAt(left) == s.charAt(right)) {
                 left++;
                 right--;
             } else {
