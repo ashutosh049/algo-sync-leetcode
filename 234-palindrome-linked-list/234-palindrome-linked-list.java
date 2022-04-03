fe9c036ea35756c817a3e4f5eaa0661ad53a83f0
ashutosh <ashutosh049@outlook.com>
2022-04-03 08:06:33 +0530

    Time: 6 ms (74.02%), Space: 98.7 MB (39.72%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -1,40 +1,55 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
     public boolean isPalindrome(ListNode head) {
-        
-        boolean isPalindrom = true;
-        
-        List<Integer> nodeList = new ArrayList<>();
-        
-        ListNode curr = head;
-        
-        while(curr != null){
-            nodeList.add(curr.val);
-            curr = curr.next;
-        }
-        
-        int left = 0;
-        int right = nodeList.size()-1;
-        
-        while(left < right){
-            if(nodeList.get(left) != nodeList.get(right)){
-                isPalindrom = false;
+        ListNode midlleNode = getMiddleNode(head);
+        ListNode secondHalfHead = reverse(midlleNode.next);
+
+        //check for palindrom for head and secondHalfHead
+        ListNode p1 = head;
+        ListNode p2 = secondHalfHead;
+        boolean isPalindrome = true;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                isPalindrome = false;
                 break;
             }
-            left++;
-            right--;
+            p1 = p1.next;
+            p2 = p2.next;
         }
-        
-        return isPalindrom;
-        
+
+        // re-reverse the second half(to revert the changes)
+        reverse(midlleNode.next);
+
+        return isPalindrome;
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    private ListNode getMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 }
