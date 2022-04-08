68203aec9469f619e556a4b1232e7a803dc9077d
ashutosh <ashutosh049@outlook.com>
2022-04-09 00:44:22 +0530

    Time: 14 ms (51.07%), Space: 42.7 MB (61.63%) - LeetHub

---
+++ 273-integer-to-english-words/273-integer-to-english-words.java
@@ -0,0 +1,45 @@
+class Solution {
+    final String[] LESS_THAN_20 = {
+        "",
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Eleven",
+        "Twelve",
+        "Thirteen",
+        "Fourteen",
+        "Fifteen",
+        "Sixteen",
+        "Seventeen",
+        "Eighteen",
+        "Nineteen"
+    };
+    final String[] TENS = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+    final String[] THOUSANDS = { "Billion", "Million", "Thousand", "" };
+    final int[] radix = { 1000000000, 1000000, 1000, 1 };
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < radix.length; i++) {
+            if (num / radix[i] == 0) continue;
+            sb.append(trans(num / radix[i])).append(THOUSANDS[i]).append(' ');
+            num %= radix[i];
+        }
+        return sb.toString().trim();
+    }
+
+    private String trans(int num) {
+        if (num == 0) return "";
+        if (num < 20) return LESS_THAN_20[num] + " ";
+        if (num < 100) return TENS[num / 10] + " " + trans(num % 10);
+        return LESS_THAN_20[num / 100] + " Hundred " + trans(num % 100);
+    }
+}
