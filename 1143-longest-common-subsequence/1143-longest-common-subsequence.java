70c794eb59602ac31c5a637d18c5b9c441f15eb5
ashutosh <ashutosh049@outlook.com>
2022-04-13 23:47:24 +0530

    Time: 115 ms (5.00%), Space: 57.6 MB (8.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,35 +1,59 @@
 class Solution {
-    
-  public int longestCommonSubsequence(String text1, String text2) {    
-    
-    // If text1 doesn't reference the shortest string, swap them.
-    if (text2.length() < text1.length()) {
-      String temp = text1;
-      text1 = text2;
-      text2 = temp;
-    }
-      
-    // The previous and current column starts with all 0's and like 
-    // before is 1 more than the length of the first word.
-    int[] previous = new int[text1.length() + 1];
-    int[] current = new int[text1.length() + 1];
-      
-    // Iterate through each column, starting from the last one.
-    for (int col = text2.length() - 1; col >= 0; col--) {
-      for (int row = text1.length() - 1; row >= 0; row--) {
-        if (text1.charAt(row) == text2.charAt(col)) {
-          current[row] = 1 + previous[row + 1];
-        } else {
-          current[row] = Math.max(previous[row], current[row + 1]);
+    Integer[][] memo;
+    int l1;
+    int l2;
+    String text1;
+    String text2;
+    String longestCommonSubsequenceString;
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        this.text1 = text1;
+        this.text2 = text2;
+        this.l1 = text1.length();
+        this.l2 = text2.length();
+        this.memo = new Integer[l1 + 1][l2 + 1]; // default, require to fill all with -1 or some dummy values
+        this.longestCommonSubsequenceString = "";
+
+        int res = 0;
+
+        //  badic checks
+        // 1. emtpy check
+        if (l1 == 0 || l2 == 0) {
+            return res;
         }
-      }
-      // The current column becomes the previous one, and vice versa.
-      int[] temp = previous;
-      previous = current;
-      current = temp;
+        // 2. single char check
+        if (l1 == 1) {
+            return text2.indexOf(text1.charAt(0)) != -1 ? 1 : 0;
+        } else if (l2 == 1) {
+            return text1.indexOf(text2.charAt(0)) != -1 ? 1 : 0;
+        }
+        // else do full check: dp
+
+        res = dp(0, 0);
+        //System.out.println("longest string: " + longestCommonSubsequenceString+ " , length: " + res);
+        return res;
     }
-        
-    // The original problem's answer is in previous[0]. Return it.
-    return previous[0];
-  }
+
+    //
+    private int dp(int i, int j) {
+        //if i have ans for i and j or i & j are at end.
+        if (i >= l1 || j >= l2) {
+            return 0;
+        }
+
+        if (memo[i][j] == null) {
+            int length = 0;
+
+            if (text1.charAt(i) == text2.charAt(j)) {
+                longestCommonSubsequenceString += text1.charAt(i);
+                length = 1 + dp(i + 1, j + 1);
+            } else {
+                length = Math.max(dp(i, j + 1), dp(i + 1, j));
+            }
+
+            memo[i][j] = length;
+        }
+
+        return memo[i][j];
+    }
 }
