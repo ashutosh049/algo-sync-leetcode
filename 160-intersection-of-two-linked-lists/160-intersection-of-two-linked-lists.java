1ee98bf56aa74ace0a4dfc0b79f715c4cc935c49
ashutosh <ashutosh049@outlook.com>
2022-04-15 03:04:57 +0530

    Time: 3 ms (31.62%), Space: 45.4 MB (83.49%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -1,48 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
         ListNode intersectionNode = null;
-        
+
         ListNode curr1 = headA;
         ListNode curr2 = headB;
-        
+
         Deque<ListNode> dq1 = new ArrayDeque<>();
         Deque<ListNode> dq2 = new ArrayDeque<>();
-        
-        while(curr1 != null){
+
+        while (curr1 != null) {
             dq1.offerFirst(curr1);
             curr1 = curr1.next;
         }
-        
-        while(curr2 != null){
+
+        while (curr2 != null) {
             dq2.offerFirst(curr2);
             curr2 = curr2.next;
         }
-        
-        while(!dq1.isEmpty() && !dq2.isEmpty()){
-            ListNode peeked1 = dq1.peek();
-            ListNode peeked2 = dq2.peek();
-            if(peeked1 == peeked2){
-                intersectionNode = dq1.peek();
-                dq1.poll();
-                dq2.poll();                
-            }else{
-                break;
-            }
+
+        while (!dq1.isEmpty() && !dq2.isEmpty() && dq1.peek() == dq2.peek()) {
+            intersectionNode = dq1.peek();
+            dq1.poll();
+            dq2.poll();
         }
-        
+
         return intersectionNode;
-        
     }
 }
