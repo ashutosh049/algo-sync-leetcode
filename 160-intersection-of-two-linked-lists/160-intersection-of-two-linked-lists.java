e238b73648e6173d33c08c3750a5d06626be2f2e
ashutosh <ashutosh049@outlook.com>
2022-04-15 03:16:15 +0530

    Time: 736 ms (5.14%), Space: 45.2 MB (87.82%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -1,28 +1,27 @@
 public class Solution {
 
+    // Brute force: cartesian plane
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode intersectionNode = null;
 
         ListNode curr1 = headA;
         ListNode curr2 = headB;
 
-        Deque<ListNode> dq1 = new ArrayDeque<>();
-        Deque<ListNode> dq2 = new ArrayDeque<>();
-
         while (curr1 != null) {
-            dq1.offerFirst(curr1);
+            ListNode temp = curr2;
+            while (temp != null) {
+                if (temp == curr1) {
+                    intersectionNode = curr1;
+                    break;
+                }
+                temp = temp.next;
+            }
+            
+            if(intersectionNode != null){
+                break;
+            }
+            
             curr1 = curr1.next;
-        }
-
-        while (curr2 != null) {
-            dq2.offerFirst(curr2);
-            curr2 = curr2.next;
-        }
-
-        while (!dq1.isEmpty() && !dq2.isEmpty() && dq1.peek() == dq2.peek()) {
-            intersectionNode = dq1.peek();
-            dq1.poll();
-            dq2.poll();
         }
 
         return intersectionNode;
