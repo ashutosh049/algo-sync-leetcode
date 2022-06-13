17edd745a0c5440af4d4b4648f87ed5cca7ec88d
ashutosh <ashutosh049@outlook.com>
2022-06-13 06:10:30 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (84.97%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,53 +1,40 @@
 class Solution {
+    int len;
+    Integer[] memoArr;
     int[] nums;
-    Map<Integer, Integer> memoMap;
+
     public int rob(int[] nums) {
         this.nums = nums;
+        this.len = nums.length;
+        this.memoArr = new Integer[this.len];
         
-        // Top-Down Memoization
-            //this.memoMap = new HashMap<>();
-            //return topDownRecursive(nums.length-1);
-        
-        // Bottom-Up Tabulation
-        return bottomUpTabulation(nums.length);
-    }
-    
-    private int bottomUpTabulation(int n){
-        
-        if(n == 1){
+        if(this.len == 1){
             return nums[0];
-        }else if(n == 2){
-            return Math.max(nums[0], nums[1]);
-        }
-        
-        int[] momoArray = new int[n];
-        momoArray[0]=nums[0];
-        momoArray[1]=Math.max(nums[0], nums[1]);
-        
-        for(int i=2; i< n; i++){
-           momoArray[i] = Math.max(momoArray[i-1], momoArray[i-2]+nums[i]);     
-        }
-        
-        return momoArray[n-1];
-    }
-    
-    private int topDownRecursive(int i){
-        if(i == 0){
-            return nums[0];
-        }else if(i == 1){
-            return Math.max(nums[0], nums[1]);
+        }else if(this.len == 2){
+            return Math.max(nums[1], nums[0]);
         }else{
-            
-            if(!memoMap.containsKey(i)){
-                int previousMax = topDownRecursive(i-1);
-                int currMax = topDownRecursive(i-2)+nums[i];
-                int maxMoney = Math.max(previousMax, currMax);
-                memoMap.put(i, maxMoney);
-            }
-            
-            return memoMap.get(i);
+            memoArr[0] = nums[0];
+            memoArr[1] = Math.max(nums[1], nums[0]);
+
+            if(len >= 2)
+                return func(len - 1);
+
+            return memoArr[len-1];            
         }
+
     }
-    
-    
+
+    Integer func(int i) {
+        if (i <= 1) {
+            return memoArr[i];
+        }
+
+        if (memoArr[i] == null) {
+            int prevAns = func(i - 1);
+            int prevToPrevAns = func(i - 2);
+            memoArr[i] = Math.max(prevAns, prevToPrevAns + nums[i]);
+        }
+
+        return memoArr[i];
+    }
 }
