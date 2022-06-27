64db4f44367addbcf7db63cca6ba242ed0d6dad3
ashutosh <ashutosh049@outlook.com>
2022-06-27 21:43:19 +0530

    Time: 4 ms (33.59%), Space: 47.8 MB (54.17%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -1,47 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode addTwoNumbers(ListNode node1, ListNode node2) {
-        
-        // L1: [9 -> 9 -> 1]
-        // L2: [1]
-        // L3: [0 -> 0 -> 2] 
-        return addNode(node1, node2, 0);
-        
+
+    public ListNode addTwoNumbers(final ListNode listNode1, final ListNode listNode2) {
+        ListNode l1 = listNode1;
+        ListNode l2 = listNode2;
+        ListNode res = new ListNode();
+        int carry = 0;
+        ListNode curr = res;
+
+        while (l1 != null || l2 != null) {
+            int v1 = (l1 == null ? 0 : l1.val);
+            int v2 = (l2 == null ? 0 : l2.val);
+
+            int sum = carry + v1 + v2;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            carry = sum / 10;
+
+            if (l1 != null) l1 = l1.next;
+
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+            curr = curr.next;
+        }
+
+        return res.next;
     }
-    
-    ListNode addNode(ListNode node1, ListNode node2, int carry){
-        
-        if(node1 == null && node2 == null){
-            if(carry > 0){
-                return new ListNode(carry);                        
-            }else {
-                return null;
-            }
-        }
-        
-        ListNode out = new ListNode(carry);
-        
-        ListNode nextNode1 = null;
-        ListNode nextNode2 = null;
-        
-        if(node1 != null){
-            out.val += node1.val;
-            nextNode1 = node1.next;
-        }
-        if(node2 != null){
-            out.val += node2.val;
-            nextNode2 = node2.next;
-        }
-        
-        if(out.val > 9){
-            int val = out.val;
-            out.val = val % 10;
-            carry = val / 10;
-        }else{
-            carry = 0;
-        }
-        
-        out.next = addNode(nextNode1, nextNode2, carry);
-        return out;
-    }
-    
 }
