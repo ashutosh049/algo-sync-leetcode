bd00e3f1219108a30ab299e47b07e56957ebf1f4
ashutosh <ashutosh049@outlook.com>
2022-07-29 21:45:51 +0530

    Time: 9 ms (48.47%), Space: 47.3 MB (5.95%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,93 +1,111 @@
 class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        //return naive(nums, target);
-        //return usingMap(nums, target);
-        return usingMapSinglePass(nums, target);
+        //return twoSum_bruteForce(nums, target);
+        //return twoSum_2pointer_fail(nums, target);
+        return twoSum_optimized(nums, target);
     }
-
-    private int[] naive(int[] nums, int target) {
-        int[] sol = new int[2];
+    
+    // O(n^2)
+    public int[] twoSum_bruteForce(int[] nums, int target) {
         int len = nums.length;
 
+        int[] sol = new int[2];
+        
         if (len == 2) {
-            // we can directl assign values, as length is only 2
             sol[0] = 0;
             sol[1] = 1;
-            return sol;
+        }else{
+            for (int i = 0; i < len; i++) {
+                int val1 = nums[i];
+
+                for (int j = i + 1; j < len; j++) {
+                    if (val1 + nums[j] == target) {
+                        // found a sol
+                        sol[0] = i;
+                        sol[1] = j;
+                    }
+                }
+            }            
         }
 
-        //2,7,11,15
-        for (int i = 0; i < len; i++) {
-            int val1 = nums[i];
-            int val2 = target - val1;
-            for (int j = i + 1; j < len; j++) {
-                if (nums[j] == val2) {
-                    sol[0] = i;
-                    sol[1] = j;
-                    break; // assume we have 1 sol only
+        return sol;
+    }
+    
+    // O(n log n)
+    public int[] twoSum_2pointer_fail(int[] nums, int target) {
+        int len = nums.length;
+
+        int[] sol = new int[2];
+        
+        if (len == 2) {
+            sol[0] = 0;
+            sol[1] = 1;
+        }else{
+            // 1. Sort: O(n*log n)
+            // 2. liner scan and use 2 pointer: O(n)
+            // Amortized: O(n log n) + o(n) ~ O(n log n)
+            
+            Arrays.sort(nums);
+            
+            Arrays.stream(nums)
+                .forEach(a -> System.out.print(a+", "));
+            
+            
+            int left = 0;
+            int right = len-1;
+            
+            while(left < right){
+                int currSum = nums[left] + nums[right];
+                System.out.println("currSum : "+currSum);
+                
+                if(currSum > target){
+                    right--;
+                }else if(currSum < target){
+                    left++;
+                }else{
+                    // found a pair
+                    sol[0] = left;
+                    sol[1] = right;
+                    break;
                 }
             }
+            // not reachable...
+            // not founf, return
+            
         }
+
         return sol;
     }
-
-    // Approach: Map.. target: O(n), SC: O(n)
-
-    private int[] usingMap(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int[] sol = new int[2];
+    
+    // Counterpart map approach
+    // TC: O(n), SC: O(n)
+    public int[] twoSum_optimized(int[] nums, int target) {
         int len = nums.length;
 
-        if (len == 2) {
-            // we can directl assign values, as length is only 2
-            sol[0] = 0;
-            sol[1] = 1;
-            return sol;
-        }
-
-        // add to map
-        for (int i = 0; i < len; i++) {
-            map.put(nums[i], i);
-        }
-
-        for (int i = 0; i < len; i++) {
-            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
-                sol[0] = i;
-                sol[1] = map.get(target - nums[i]);
-                break;
-            }
-        }
-        return sol;
-    }
-
-    // Approach 3: 1 pass hash map.. target: O(n), SC: O(n)
-
-    private int[] usingMapSinglePass(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
         int[] sol = new int[2];
-        int len = nums.length;
-
+        
         if (len == 2) {
-            // we can directl assign values, as length is only 2
             sol[0] = 0;
             sol[1] = 1;
-            return sol;
+        }else{
+            
+            Map<Integer, Integer> counterpartMap = new HashMap<>();
+            
+            for(int i =0; i < len; i++){
+                int val = nums[i];
+                int counterpart = target- val;
+                if(counterpartMap.containsKey(counterpart)){
+                    // found a sol
+                     sol[0] = i;
+                     sol[1] = counterpartMap.get(counterpart);
+                }else{
+                    counterpartMap.put(val, i);
+                }
+            }
+            
         }
 
-        // check and add to map
-        for (int i = 0; i < len; i++) {
-            if (map.containsKey(target - nums[i])) {
-                // got a pair
-                sol[0] = i;
-                sol[1] = map.get(target - nums[i]);
-                break;
-            } else {
-                map.put(nums[i], i);
-            }
-        }
         return sol;
     }
 }
